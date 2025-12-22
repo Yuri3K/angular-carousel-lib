b7449441701c5c8623326aaf099843d40181dd9d
Yuri_S <zebrawild@gmail.com>
2025-12-22 23:51:44 +0200

    test

--- projects/carousel/src/lib/components/ngx-carousel-controls/ngx-carousel-controls.component.ts
+++ projects/carousel/src/lib/components/ngx-carousel-controls/ngx-carousel-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgxCarouselService } from '../../services/ngx-carousel.service';
 
 @Component({
@@ -7,6 +7,33 @@ import { NgxCarouselService } from '../../services/ngx-carousel.service';
   templateUrl: './ngx-carousel-controls.component.html',
   styleUrl: './ngx-carousel-controls.component.scss',
 })
-export class NgxCarouselControlsComponent {
-  carousel = inject(NgxCarouselService)
+export class NgxCarouselControlsComponent implements OnInit {
+  carousel = inject(NgxCarouselService);
+
+  private resizeObserver!: ResizeObserver;
+  widthTest = 0
+
+  ngOnInit() {
+    console.log('INIT');
+    setTimeout(() => {
+      this.watchResize();
+      
+    }, 3000);
+    setInterval(() =>   {
+      console.log("TEST", this.widthTest)
+    }, 100)
+  }
+
+  private watchResize() {
+    console.log('IN');
+
+    this.resizeObserver = new ResizeObserver((entries) => {
+      const width = entries[0].contentRect.width;
+      console.log('🚀 ~ width:', width);
+      this.widthTest = width
+      // this.layout.setCarouselWidth(width);
+      // this.state.setWidth(width);
+    });
+    console.log("🚀 ~ this.resizeObserver:", this.resizeObserver)
+  }
 }
--- projects/carousel/src/lib/ngx-carousel.types.ts
+++ projects/carousel/src/lib/ngx-carousel.types.ts
@@ -1,4 +1,8 @@
-import { InjectionToken } from "@angular/core"
+import { InjectionToken } from '@angular/core';
+
+export interface NgxCarouselBreakpoint extends NgxCarouselConfig {
+  breakpoint: number; // Ширина экрана, при которой применяется конфигурация (max-width)
+}
 
 export interface NgxCarouselConfig {
   autoplay?: boolean,
@@ -7,15 +11,29 @@ export interface NgxCarouselConfig {
   pauseOnHover?: boolean;
   animation?: 'slide' | 'fade',
   startIndex?: number,
+  slidesToShow?: number
+  showArrows?: boolean;
+  showDots?: boolean;
+  spaceBetween?: number;
+  speed?: number;
+  breakpoints?: NgxCarouselBreakpoint[],
 }
 
 export const DEFAULT_CAROUSEL_CONFIG: NgxCarouselConfig = {
-  autoplay: true,
-  interval: 5000,
-  loop: true,
-  pauseOnHover: true,
-  animation: 'slide',
-  startIndex: 0,
+  autoplay: true, // автопроигрывание слайдов
+  interval: 5000, // время переключения слайдов
+  loop: true,     // бесконечная прокрутка
+  pauseOnHover: true, // останавливать автопрокрутку при наведении
+  animation: 'slide',  // тип анимации
+  startIndex: 0,      // начальный номер слайда
+  slidesToShow: 1,     // по умолчанию показываем 1 слайд
+  showArrows: true,
+  showDots: true,
+  spaceBetween: 0,
+  speed: 500,
+  breakpoints: [],     // по умолчанию нет брейкпоинтов
 }
 
-export const NGX_CAROUSEL_CONFIG = new InjectionToken<NgxCarouselConfig>('NGX_CAROUSEL_CONFIG')
+export const NGX_CAROUSEL_CONFIG = new InjectionToken<NgxCarouselConfig>(
+  'NGX_CAROUSEL_CONFIG'
+);
